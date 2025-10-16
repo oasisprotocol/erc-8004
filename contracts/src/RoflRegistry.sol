a9529181e85b2555715a15d9680f803d488b2ea8
Matevž Jekovec <matevz@oasisprotocol.org>
2025-10-16 09:30:05 +0200

    contracts: address -> pubkey

--- contracts/src/RoflRegistry.sol
+++ contracts/src/RoflRegistry.sol
@@ -4,20 +4,20 @@ pragma solidity ^0.8.0;
 import { Subcall } from "@oasisprotocol/sapphire-contracts/contracts/Subcall.sol";
 
 contract RoflRegistry {
-    event KeyAdded(bytes21, string, address);
+    event KeyAdded(bytes21, string, bytes32);
 
-    mapping(bytes => address) private _keys;
+    mapping(bytes => bytes32) private _keys;
 
-    function addKey(address pubkey, string calldata context) external {
+    function addKey(bytes32 pubkey, string calldata context) external {
         _keys[abi.encodePacked(Subcall.getRoflAppId(), context)] = pubkey;
 
         emit KeyAdded(Subcall.getRoflAppId(), context, pubkey);
     }
-    function getKey(bytes21 appId, string calldata context) external view returns (address) {
+    function getKey(bytes21 appId, string calldata context) external view returns (bytes32) {
         return _keys[abi.encodePacked(appId, context)];
     }
 
     function hasKey(bytes21 appId, string calldata context) external view returns (bool) {
-        return _keys[abi.encodePacked(appId, context)] != address(0);
+        return _keys[abi.encodePacked(appId, context)] != bytes32(0);
     }
 }
--- contracts/test/RoflRegistry.t.sol
+++ contracts/test/RoflRegistry.t.sol
@@ -8,7 +8,7 @@ import {SapphireTest} from "@oasisprotocol-sapphire-foundry-0.1.2/BaseSapphireTe
 contract RoflRegistryTest is SapphireTest {
     RoflRegistry public registry;
 
-    address testPubkey = address(0x1234567890123456789012345678901234567890);
+    bytes32 testPubkey = bytes32(0x1234567890123456789012345678901234567890123456789012345678901234);
     string testContext = "test-context";
     bytes21 testAppId = bytes21(0x000000000000000000000000000000000000000000);
 
@@ -22,32 +22,32 @@ contract RoflRegistryTest is SapphireTest {
 
         registry.addKey(testPubkey, testContext);
 
-        address pk = registry.getKey(testAppId, testContext);
+        bytes32 pk = registry.getKey(testAppId, testContext);
         assertEq(pk, testPubkey);
         assertTrue(registry.hasKey(testAppId, testContext));
     }
 
     function testAddKeyMultipleContexts() public {
-        address testPubkey2 = address(0x9876543210987654321098765432109876543210);
+        bytes32 testPubkey2 = bytes32(0x9876543210987654321098765432109876543210987654321098765432104321);
         string memory context2 = "another-context";
 
         registry.addKey(testPubkey, testContext);
         registry.addKey(testPubkey2, context2);
 
-        address pk1 = registry.getKey(testAppId, testContext);
-        address pk2 = registry.getKey(testAppId, context2);
+        bytes32 pk1 = registry.getKey(testAppId, testContext);
+        bytes32 pk2 = registry.getKey(testAppId, context2);
 
         assertEq(pk1, testPubkey);
         assertEq(pk2, testPubkey2);
     }
 
     function testOverwriteKey() public {
-        address newPubkey = address(0x1111111111111111111111111111111111111111);
+        bytes32 newPubkey = bytes32(0x1111111111111111111111111111111111111111111111111111111111111111);
 
         registry.addKey(testPubkey, testContext);
         registry.addKey(newPubkey, testContext);
 
-        address key = registry.getKey(testAppId, testContext);
+        bytes32 key = registry.getKey(testAppId, testContext);
         assertEq(key, newPubkey);
     }
 }
